9d954c9d05c057632f1f358868bd8223f4eaa841
Jonathan Santo <jonathanoesanto@outlook.com>
2022-05-10 20:56:41 -0300

    explicação sobre const

--- Constantes/Aula002.js
+++ Constantes/Aula002.js
@@ -5,3 +5,5 @@ console.log(valorIngressoAdulto);
 
 
 // Let permite que você redefina quantas vezes precisar
+// const não pode ser redefinida como indica o nome
+// preferencialmente utilizar o const e somente utilizar o let quando tiver a necessidade de alteração de valores (boa prática).
