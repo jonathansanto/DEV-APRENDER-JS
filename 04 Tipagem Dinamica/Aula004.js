b05b77c47c40feb1b191f71ff8cb70d45b9934b5
Jonathan Santo <jonathanoesanto@outlook.com>
2022-05-10 21:17:46 -0300

    Tipagem dinamica

---
+++ 04 Tipagem Dinamica/Aula004.js
@@ -0,0 +1,7 @@
+// Nós não precisamos dizer para o JavaScript que cliente é uma String, ele já sabe que é!
+
+let nome = 'Jonathan';
+let idade = 25;
+let estaAprovado = true;
+let nobrenome = undefined;
+let corSelecionado = null;
