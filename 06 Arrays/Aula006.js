c2c51ca16b6123c67a6dc4dc1e64752aaf966e68
Jonathan Santo <jonathanoesanto@outlook.com>
2022-05-10 21:38:41 -0300

    arrays

---
+++ 06 Arrays/Aula006.js
@@ -0,0 +1,9 @@
+// Conjunto de dados que pode ser acessado por um indice
+// indíce - posição exata onde se encontra uma informação dentro do conjunto de informações
+
+
+let familia = [25,45,'jonathan',10];
+console.log(familia.length);
+console.log(familia[2]);
+
+let nomeDoColega = [];
