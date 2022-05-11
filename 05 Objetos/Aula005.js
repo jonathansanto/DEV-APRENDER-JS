63401655a08632e19f3af4157da30aad7985e0c9
Jonathan Santo <jonathanoesanto@outlook.com>
2022-05-10 21:26:14 -0300

    Objetos

---
+++ 05 Objetos/Aula005.js
@@ -0,0 +1,14 @@
+let nome = 'Jonathan';
+let idade = 22;
+let estaAprovado = true;
+let sobrenome = undefined;
+
+let pessoa = {
+    nome: 'Jonathan',
+    idade: 22,
+    estaAprovado: true,
+    sobrenome : 'Santo'
+ };
+
+
+ console.log(pessoa);
