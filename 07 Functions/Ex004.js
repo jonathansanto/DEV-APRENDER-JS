0b65fd9b4dd65ca90c2ea106400eac197f92fe59
Jonathan Santo <jonathanoesanto@outlook.com>
2022-05-10 22:07:34 -0300

    Excercicio função

---
+++ 07 Functions/Ex004.js
@@ -0,0 +1,12 @@
+// Monte uma função, exiba no console, altere algo e exiba novamente.
+
+let nome1 = "Jonathan";
+function transformarNome (nome, sobrenome){
+  nome1 = nome + ' ' + sobrenome;
+
+
+};
+
+console.log(nome1);
+transformarNome ("Jonathan", "Santo");
+console.log(nome1);
