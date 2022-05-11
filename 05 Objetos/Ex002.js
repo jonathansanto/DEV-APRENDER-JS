9b5c5c7da99b101be99872671b920afeddc9347e
Jonathan Santo <jonathanoesanto@outlook.com>
2022-05-10 21:31:17 -0300

    Exercicio objeto

---
+++ 05 Objetos/Ex002.js
@@ -0,0 +1,13 @@
+// Crie um objeto com 3 propriedades e exiba no console
+
+let nomePet = 'Tobias';
+let idade = 2;
+let eCastrado = true;
+
+let pet = {
+    nomePet: 'Tobias',
+    idade: 2,
+    eCastrado: true
+};
+
+console.log(pet);
