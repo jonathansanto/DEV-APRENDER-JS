25360a8b209b73d0e27c27dfbb993a1f3cd74565
Jonathan Santo <jonathanoesanto@outlook.com>
2022-05-11 07:55:36 -0300

    Tipos de funções

---
+++ 07 Functions/Aula008.js
@@ -0,0 +1,19 @@
+// Tipos de funções - realiza uma tarefa e não devolve nada
+
+function dizerNome(){
+    console.log('jonathan');
+
+};
+
+dizerNome();
+
+//Faz um cálculo ou operação e retorna algo
+function multiplicarPorDois (valor){
+    return valor*2;
+};
+
+//console.log(multiplicarPorDois(5));
+
+let resultado = multiplicarPorDois(5);
+
+console.log(resultado);
