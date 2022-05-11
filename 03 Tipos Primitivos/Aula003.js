0b124ef76aea9436cf93ff6d09d5835449972d7f
Jonathan Santo <jonathanoesanto@outlook.com>
2022-05-10 21:08:43 -0300

    Tipos primitivos

---
+++ 03 Tipos Primitivos/Aula003.js
@@ -0,0 +1,8 @@
+//Tipos "referências" e "valores" 
+
+
+let nome = 'Jonathan'; // String literal
+let idade = 22; // Number literal
+let estaAprovado = true; // Boolean 
+let sobrenome ; // Undefined
+let corSelecionado = null; // Redefinir um valor
